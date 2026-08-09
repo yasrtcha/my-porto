f41c308b9db5f17f7c6ca7c283652fccc55d91cb
yasrtcha-create <danarraisa@gmail.com>
2026-08-09 13:18:31 +0700

    opotimize hero image size and set high fetch priority

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -110,15 +110,17 @@ export default function Hero() {
             <PixelTransition
               firstContent={
                 <img
-                  src="https://res.cloudinary.com/oyuxswwy/image/upload/v1785603093/ppp_ngtadd.jpg"
+                  src="https://res.cloudinary.com/oyuxswwy/image/upload/f_auto,q_auto/v1785603093/ppp_ngtadd.jpg"
                   alt="default pixel transition content, a cat!"
+                  fetchPriority="high"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               }
               secondContent={
                 <img
-                  src="https://res.cloudinary.com/oyuxswwy/image/upload/v1785603107/cat_it_pkkqqk.jpg"
+                  src="https://res.cloudinary.com/oyuxswwy/image/upload/f_auto,q_auto/v1785603107/cat_it_pkkqqk.jpg"
                   alt="default pixel transition content, a cat!"
+                  fetchPriority="high"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               }
